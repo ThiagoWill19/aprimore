8802dfb8419860c0734aa44ddff60c0db0d34f4c
ThiagoWill19 <thiagopompeu19@gmail.com>
2026-01-14 18:15:58 -0300

    Created User and Business

--- src/main/java/com/aprimore/models/Business.java
+++ src/main/java/com/aprimore/models/Business.java
@@ -5,12 +5,10 @@ import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,28 +20,17 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Client {
+public class Business {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 	
-	@Column(name="name")
-	private String clientName;
+	private String name;
 	
-	@Column(name="email")
-	private String clientEmail;
+	@OneToMany(mappedBy = "business", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Client> clients = new ArrayList<>();
 	
-	@Column(name="phone_number")
-	private String clientPhoneNumber;
-	
-	@ManyToOne
-	private Business business;
-	
-	@OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Machine> machines = new ArrayList<>();
-	
-	@OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<ServiceOrder> serviceOrders = new ArrayList<>();
-	
+	@OneToMany(mappedBy = "business", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<User> users = new ArrayList<>();
 }
---
+++ src/main/java/com/aprimore/models/User.java
@@ -0,0 +1,34 @@
+package com.aprimore.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	
+	private String name;
+	
+	private String password;
+	
+	private String email;
+	
+	@ManyToOne
+	private Business business;
+}
