f92d7932377ac8c042b20313308e68cbe28cde11
ThiagoWill19 <thiagopompeu19@gmail.com>
2026-05-22 10:33:32 -0300

    fix: move admin credentials to environment variables

--- src/main/java/com/aprimore/configurations/init/DataInit.java
+++ src/main/java/com/aprimore/configurations/init/DataInit.java
@@ -1,34 +1,52 @@
 package com.aprimore.configurations.init;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.aprimore.models.User;
 import com.aprimore.models.enuns.AccountStatus;
 import com.aprimore.models.enuns.Role;
 import com.aprimore.repositories.UserRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Slf4j
 @Configuration
 public class DataInit {
+
+	private final PasswordEncoder passwordEncoder;
+    
+	@Value("${main.email}")
+	private String mainEmail;
+	@Value("${main.pass}")
+	private String mainPass;
+	@Value("${main.name}")
+	private String mainName;
+
+    DataInit(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
 	@Bean
 	public CommandLineRunner init(UserRepository userRepository) {
 		return args -> {
 			//Verifica se já existe um admin
-			if(userRepository.findByEmail("admin@email.com").isEmpty()) {
+			if(userRepository.findByEmail(mainEmail).isEmpty()) {
 				User user = new User();
-				user.setName("Thiago");
-				user.setEmail("admin@email.com");
-				user.setPassword(new BCryptPasswordEncoder().encode("admin123"));
+				user.setName(mainName);
+				user.setEmail(mainEmail);
+				user.setPassword(passwordEncoder.encode(mainPass));
 				user.setRole(Role.ADMIN);
 				user.setAccountStatus(AccountStatus.ACTIVE);
 				userRepository.save(user);
-				System.out.println("✅ admin criado com sucesso.");
-				System.out.println("Email: " + user.getEmail());
+				log.info("✅ admin criado com sucesso.");
+				log.info("Email: " + user.getEmail());
 			}else {
-				System.out.println("ℹ️ Admin já existe.");
+				log.info("ℹ️ Admin já existe.");
 			}
 		};
 	}
